78c1897dc15a691e9751e20a4c0341f64e4bd15b
RymndH <147457269+RymndH@users.noreply.github.com>
2023-10-17 14:38:49 -0700

    Update Session 1 ASTR 19.py

--- Session 1 ASTR 19.py
+++ Session 1 ASTR 19.py
@@ -1,3 +1,12 @@
-print("My name is Raymond Huang.")
-print("My favorite movie is Ratatouille.")
-print("My favorite food is Braised Pork Belly.")
+def main():
+
+
+  def printf():
+    print("My name is Raymond Huang")
+    print("My favorite movie is Ratatouille.")
+    print("My favorite food is braised pork.")
+
+  printf()
+
+if __name__ == "__main__":
+  main()
