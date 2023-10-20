391a2f6cdfd593faba6fb7875ac628bd6067833e
RymndH <147457269+RymndH@users.noreply.github.com>
2023-10-20 16:23:59 -0700

    Update ASTRS1.py
    Added Comments

--- ASTRS1.py
+++ ASTRS1.py
@@ -1,6 +1,6 @@
 def main():
 
-
+  #Function that prints my name, favorite movie, and favorite food in seperate lines.
   def printf():
     print("My name is Raymond Huang")
     print("My favorite movie is Ratatouille.")
